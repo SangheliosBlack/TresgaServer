e198b5ddbdcfb383f50e4a3794df1dc3359c043d
jvillagrana <jvillagrana@softnet.mx>
2024-01-10 08:18:57 -0600

    ad vales controller

--- controllers/vales.js
+++ controllers/vales.js
@@ -2,6 +2,101 @@ const catchAsync = require("../utils/catchAsync");
 const Vales = require('../models/vales');
 
 const controller = {
+    createNewValesList:catchAsync(async(req,res,next)=>{
+      const params = req.body;
+
+    
+    var folio_inicial = +params.folio_inicial;
+    const folio_final = +params.folio_final;
+    const producto = params.producto;
+    const monto = params.monto;
+
+    var string_trim = monto.trim();
+    var string_2 = string_trim.replace(",",".");
+    var string_3 = string_2.replace(".","");
+    var string_4 = string_3.replace("$","");
+    
+    var array_vales = [];
+    var sub_modelo = {};
+    var folio_cast = "";
+
+    var tipo = false;
+    if(producto == "DIESEL"){
+      tipo = true;
+      string_4 = " ";
+    }else{
+      +string_4
+    }
+    if(producto == 'FACTURA'){
+      params.factura = true;
+    }else{
+      params.factura = false;
+    }
+
+    var fecha = moment().format('lll');
+
+    for(var i = folio_inicial; i <= folio_final ;i++){      
+      folio_cast = i.toString();
+      if(producto == 'FACTURA'){
+        if(params.razon == 'G'){
+          folio_cast = 'G'+folio_cast;
+        }else if(params.razon == 'F'){
+          folio_cast = 'F'+folio_cast;
+        }
+      }
+      sub_modelo ={
+        "folio":folio_cast,
+        "producto":producto,
+        "importe":string_4,
+        "numero":i,
+        "usado":false,
+        "tipo":tipo,
+        "cancelado":false,
+        "numero_rest":i,
+        "factura":params.factura,
+      }
+      array_vales.push(sub_modelo);
+    }
+
+    var aprobado = true;
+    var num = 0;
+
+    console.log(array_vales);
+
+    Vales.find(function(err,data){
+      if(!err){
+        array_vales.forEach(function(element,index,array){
+          num ++;
+          data.forEach(function(element2){
+            if(element2.folio == element.folio){
+              aprobado = false;
+            }else{
+
+            }
+          });
+          if(num == array.length ){
+            if(aprobado){
+              Vales.insertMany(array_vales,function(err,data){
+                if(err){
+                }else{
+                 return res.status(200).json({response:"CREATE NEW LIST DONE",aprobado});
+                }
+              });
+            }else{
+              return res.status(200).json({response:"CREATE NEW LIST FAILURE",aprobado});
+            }
+          }
+        });
+      }
+    });
+    }),
+    cancelVale: catchAsync(async(req,res,next)=>{
+      Vales.findByIdAndUpdate(req.body.id,{cancelado:true}, function (err, data) {
+        if (!err) {
+          return res.redirect("/vales");
+        }
+      });
+    }),
     getVales: catchAsync(async(req,res,next)=>{
         Vales.aggregate([
             {
